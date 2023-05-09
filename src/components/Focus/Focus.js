5c21bf857e1407e8b2c7b70ca74c69d500b4ea28
Dania <dabeaher@yahoo.com>
2023-05-09 17:01:00 -0400

    Restructure focus component

--- src/components/Focus/Focus.js
+++ src/components/Focus/Focus.js
@@ -1,23 +1,37 @@
 import './Focus.scss';
-import house from '../../assets/images/focus-photo.png';
+import money from '../../assets/icons/money-white.png';
+import checklist from '../../assets/icons/checklist_white_24dp.svg';
+import book from '../../assets/icons/menu_book_white_24dp.svg';
+import stocks from '../../assets/icons/stock-market-white.png';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 function Focus() {
     //provides animate-on-scroll
     Aos.init({
-        duration: 800,
+        duration: 600,
         easing: 'ease-in'
     });
 
     return (
         <section className='focus'>
-            <img className='focus__img' data-aos='fade-left' src={house} alt='miniature house next to keys' />
-            <div>
-                <h2 data-aos='fade-left' data-aos-delay='400'>FOCUS ON THE IMPORTANT THINGS</h2>
-                <p className='focus__p' data-aos='fade-left' data-aos-delay='600'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+            <img className='focus__top-img' src={money} alt='dollar bills' />
+            <h2 data-aos='fade-left'>FINANCE AT YOUR FINGERTIPS</h2>
+            <p className='focus__p' data-aos='fade-left'>Follow our straightforward approach to learning.</p>
+            <div className='focus__instructions'>
+                <div className='focus__instructions--single' data-aos='fade-down'>
+                    <img src={checklist} alt='checklist' />
+                    <p>Build a focused list of your most important financial goals.</p>
+                </div>
+                <div className='focus__instructions--single' data-aos='fade-down'>
+                    <img src={book} alt='open book' />
+                    <p>Work through a library of lessons curated specifically for you.</p>
+                </div>
+                <div className='focus__instructions--single' data-aos='fade-down'>
+                    <img className='focus__icon' src={stocks} alt='rising stock' />
+                    <p>Use your learnings in realistic, simulated practice.</p>
+                </div>
             </div>
-
         </section>
     );
 }
--- src/components/Mission/Mission.js
+++ src/components/Mission/Mission.js
@@ -6,7 +6,7 @@ import 'aos/dist/aos.css';
 function Mission() {
     //provides animate-on-scroll
     Aos.init({
-        duration: 800,
+        duration: 600,
         easing: 'ease-in'
     });
 
@@ -14,8 +14,8 @@ function Mission() {
         <section className='mission'>
             <img className='mission__img' data-aos='fade-right' src={coinman} alt='man reading atop large coins' />
             <div>
-                <h2 data-aos='fade-right' data-aos-delay='400'>OUR MISSION</h2>
-                <p className='mission__p' data-aos='fade-right' data-aos-delay='600'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+                <h2 data-aos='fade-right' data-aos-delay='200'>STEPPING TOWARD FINANCIAL ACCESSIBILITY</h2>
+                <p className='mission__p' data-aos='fade-right' data-aos-delay='400'>JadeSmart is built with the average, everyday individual in mind. We create comprehensible and interactive financial literacy lessons to break down the barrier between you and the "experts".</p>
             </div>
         </section>
     );
