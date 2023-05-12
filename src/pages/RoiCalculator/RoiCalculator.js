aa516ceb6deb99f92d0e7ab55db7db6d665ac783
Dania <dabeaher@yahoo.com>
2023-05-12 14:04:48 -0400

    API data successfully populates the chart, created function that converts the date

--- src/pages/RoiCalculator/RoiCalculator.js
+++ src/pages/RoiCalculator/RoiCalculator.js
@@ -13,47 +13,46 @@ function RoiCalculator() {
 
     //This axios call retrieves stock data from finnhub. Arguments needed are stock symbol, resolution (60, D, W, M), and timeframe in unix
     useEffect(() => {
-        axios.get(`${baseUrl}stock/candle?symbol=AAPL&resolution=D&from=1680307200&to=1682899140&token=${API_key}`)
-            .then(response => {
-                setStockData(response.data);
-            })
+        axios.get(`${baseUrl}stock/candle?symbol=AAPL&resolution=M&from=1526083200&to=1683849600&token=${API_key}`)
+        .then(response => {
+            const formattedData = response.data.c.map((value, index) => ({
+              c: value,
+              t: response.data.t[index]
+            }));
+
+            setStockData(formattedData);            
+        })
             .catch(error => {
                 console.log(error);
             });
     }, [baseUrl, API_key]);
 
-    console.log(stockData);
+    // console.log(stockData);
+    
+    const convertedDate = (array) => {
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            const date = dayjs.unix(element.t).format('MMM/YYYY');
+            console.log(date);
+        }
+    }
 
-    const data = [
-        {
-            date: dayjs.unix(1680480000).format('MM/DD'),
-            price: 4000,
-        },
-        {
-            date: dayjs.unix(1680566400).format('MM/DD'),
-            price: 3000,
-        },
-        {
-            date: dayjs.unix(1680652800).format('MM/DD'),
-            price: 2000,
-        },
-        {
-            date: dayjs.unix(1680739200).format('MM/DD'),
-            price: 2780,
-        },
-        {
-            date: dayjs.unix(1681084800).format('MM/DD'),
-            price: 1890,
-        },
-        {
-            date: dayjs.unix(1681171200).format('MM/DD'),
-            price: 2390,
-        },
-        {
-            date: dayjs.unix(1681257600).format('MM/DD'),
-            price: 3490,
-        },
-    ];
+    convertedDate(stockData);
+
+    // const data = [
+    //     {
+    //         date: dayjs.unix(1680480000).format('MM/DD'),
+    //         price: 4000,
+    //     },
+    //     {
+    //         date: dayjs.unix(1680566400).format('MM/DD'),
+    //         price: 3000,
+    //     },
+    //     {
+    //         date: dayjs.unix(1680652800).format('MM/DD'),
+    //         price: 2000,
+    //     },
+    // ];
 
     return (
         <main className='stocks'>
@@ -62,20 +61,20 @@ function RoiCalculator() {
                     <LineChart
                         width={500}
                         height={300}
-                        data={data}
+                        data={stockData}
                         margin={{
                             top: 5,
                             right: 30,
-                            left: 20,
+                            left: 0,
                             bottom: 5,
                         }}
                     >
-                        <XAxis dataKey="date" stroke='#EDF7F6' tickLine={false} />
-                        <YAxis stroke='#EDF7F6' hide={true} />
+                        <XAxis dataKey="t" stroke='#EDF7F6' tickLine={false} />
+                        <YAxis stroke='#EDF7F6' tickLine={false} />
                         <Tooltip 
                         labelStyle={{ color: '#EDF7F6' }}
                         contentStyle={{ backgroundColor: '#122535' }} />
-                        <Line dataKey="price" stroke="#01B36C" dot={false} />
+                        <Line dataKey="c" stroke="#01B36C" dot={false} />
                     </LineChart>
                 </ResponsiveContainer>
             </div>
