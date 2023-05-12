123ba7ec4e3e574f360bc4de5630958102430242
Dania <dabeaher@yahoo.com>
2023-05-12 15:56:17 -0400

    Adjust fade animation

--- src/components/Focus/Focus.js
+++ src/components/Focus/Focus.js
@@ -9,25 +9,25 @@ import 'aos/dist/aos.css';
 function Focus() {
     //provides animate-on-scroll
     Aos.init({
-        duration: 600,
+        duration: 800,
         easing: 'ease-in'
     });
 
     return (
         <section className='focus'>
             <img className='focus__top-img' src={money} alt='dollar bills' />
-            <h2 data-aos='fade-left'>FINANCE AT YOUR FINGERTIPS</h2>
-            <p className='focus__p' data-aos='fade-left'>Follow our straightforward approach to learning.</p>
-            <div className='focus__instructions'>
-                <div className='focus__instructions--single' data-aos='fade-down'>
+            <h2 data-aos='fade-up'>FINANCE AT YOUR FINGERTIPS</h2>
+            <p className='focus__p' data-aos='fade-up'>Follow our straightforward approach to learning.</p>
+            <div className='focus__instructions' data-aos='fade-up'>
+                <div className='focus__instructions--single'>
                     <img src={checklist} alt='checklist' />
                     <p>Build a focused list of your most important financial goals.</p>
                 </div>
-                <div className='focus__instructions--single' data-aos='fade-down'>
+                <div className='focus__instructions--single'>
                     <img src={book} alt='open book' />
                     <p>Work through a library of lessons curated specifically for you.</p>
                 </div>
-                <div className='focus__instructions--single' data-aos='fade-down'>
+                <div className='focus__instructions--single'>
                     <img className='focus__icon' src={stocks} alt='rising stock' />
                     <p>Use your learnings in realistic, simulated practice.</p>
                 </div>
--- src/components/Mission/Mission.js
+++ src/components/Mission/Mission.js
@@ -6,16 +6,16 @@ import 'aos/dist/aos.css';
 function Mission() {
     //provides animate-on-scroll
     Aos.init({
-        duration: 600,
+        duration: 800,
         easing: 'ease-in'
     });
 
     return (
         <section className='mission'>
-            <img className='mission__img' data-aos='fade-right' src={coinman} alt='man reading atop large coins' />
+            <img className='mission__img' data-aos='fade-up' src={coinman} alt='man reading atop large coins' />
             <div>
-                <h2 data-aos='fade-right' data-aos-delay='200'>STEPPING TOWARD FINANCIAL ACCESSIBILITY</h2>
-                <p className='mission__p' data-aos='fade-right' data-aos-delay='400'>JadeSmart is built with the average, everyday individual in mind. We create comprehensible and interactive financial literacy lessons to break down the barrier between you and the "experts".</p>
+                <h2 data-aos='fade-up' data-aos-delay='200'>STEPPING TOWARD FINANCIAL ACCESSIBILITY</h2>
+                <p className='mission__p' data-aos='fade-up' data-aos-delay='400'>JadeSmart is built with the average, everyday individual in mind. We create comprehensible and interactive financial literacy lessons to break down the barrier between you and the "experts".</p>
             </div>
         </section>
     );
