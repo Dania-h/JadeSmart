68d8c54a975e34eab27801e22cf10d331ca2fece
Dania <dabeaher@yahoo.com>
2023-05-14 01:58:08 -0400

    Finish mobile lessons page header

--- src/pages/Lessons/Lessons.js
+++ src/pages/Lessons/Lessons.js
@@ -1,9 +1,10 @@
 import StockChart from "../StockChart/StockChart";
+import LessonsHeader from "./components/LessonsHeader/LessonsHeader";
 
 function Lessons() {
     return (
         <>
-            <StockChart />
+            <LessonsHeader />
         </>
     );
 }
---
+++ src/pages/Lessons/components/LessonsHeader/LessonsHeader.js
@@ -0,0 +1,19 @@
+import './LessonsHeader.scss'
+import logo from '../../../../assets/icons/jadesmart-logo--icon.svg'
+import { Link } from 'react-router-dom';
+
+function LessonsHeader() {
+    return (
+        <header className='header'>
+            <Link className='header__links' to="/">
+                <img className='header__logo' src={logo} alt='jade green gem' />
+                <h2 className='header__link'>JadeSmart</h2>
+            </Link>
+            <Link to="#">
+                <button className='header__button'>Login</button>
+            </Link>
+        </header>
+    );
+}
+
+export default LessonsHeader;
